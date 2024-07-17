42774ae6416db8177edc6663a6d2260871a4dd1a
RicardoClaro7 <167456148+RicardoClaro7@users.noreply.github.com>
2024-07-17 08:10:48 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,38 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { Button } from "bootstrap";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let myNumbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let myEmojis = ["♠", "♥", "♣", "♦"];
+
+  let myRandomNumber = Math.floor(Math.random() * myNumbers.length);
+  let myRandomEmojis = Math.floor(Math.random() * myEmojis.length);
+
+  let selectorNumber = myNumbers[myRandomNumber];
+  let selectorEmojis = myEmojis[myRandomEmojis];
+
+  let primerPalo = document.getElementById("palo1");
+  primerPalo.innerHTML = selectorEmojis;
+
+  let segundoPalo = document.getElementById("palo2");
+  segundoPalo.innerHTML = selectorEmojis;
+
+  let number = document.getElementById("numero");
+  number.innerHTML = selectorNumber;
 };
